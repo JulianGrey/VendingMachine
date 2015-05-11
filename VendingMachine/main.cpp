54c14f5c16a4d14779ac84b2a05a3dec1335ba04
Julian Grey <julianrgrey@gmail.com>
2015-05-11 12:24:46 +0100

    Basic working implementation of VendingMachine app

--- VendingMachine/main.cpp
+++ VendingMachine/main.cpp
@@ -1,38 +1,51 @@
+#include "drink.hpp"
 #include <iostream>
+#include <string>
+#include <vector>
 
-#define COKE 1
-#define SPRITE 2
-#define TANGO 3
-#define WATER 4
+void addDrink(std::vector<Drink>* v); // Handle adding drinks manually
+void loadStock(); // Load stock from file
 
-void availableDrinks(); // Get a list of drinks which are in stock
-void dispense(int drink); // Dispend a selected drink
-void restock(int drink, int amount);
+int main() {
+	std::vector<Drink>* vDrink = new std::vector<Drink>;
+	Drink coke("Coke", 1.35, 10, vDrink);
+	(*vDrink).push_back(coke);
+	Drink sprite("Sprite", 1.05, 0, vDrink);
+	(*vDrink).push_back(sprite);
+	Drink fanta("Fanta", 1.25, 10, vDrink);
+	(*vDrink).push_back(fanta);
 
-double calculate(); // total cost of purchase
+	std::cout << "Welcome. What drink would you like to buy?\n\n";
+	for(int i = 0; i < (*vDrink).size(); i++) {
+		// Get a list of drinks which are in stock
+		if((*vDrink)[i].stock > 0) {
+			std::cout << (*vDrink)[i].name << " - " << (*vDrink)[i].price;
+			std::cout << " (selection: " << (*vDrink)[i].id << ")\n";
+		}
+	}
+	std::cout << '\n';
 
-int main() {std::cout << "Welcome. What drink would you like to buy?\n\n";
-	availableDrinks();
+	bool* found = new bool;
+	int* selection = new int;
+	std::cin >> *selection;
+	*found = false;
 
-	int selection;
-	std::cin >> selection;
-
-	switch(selection) {
-		case COKE:
-			dispense(COKE);
+	for(int i = 0; i < (*vDrink).size(); i++) {
+		if((*vDrink)[i].id == *selection) {
+			*found = true;
+			(*vDrink)[i].removeStock(); // This process handles out of stock
 			break;
-		case SPRITE:
-			dispense(SPRITE);
-			break;
-		case TANGO:
-			dispense(TANGO);
-			break;
-		case WATER:
-			dispense(WATER);
-			break;
-		default:
-			std::cout << "Invalid selection, choose again\n\n";
+		}
+	}
+	if(!(*found)) {
+		// Drink ID doesn't exist
+		std::cout << "Invalid selection.\n";
 	}
 
+	delete found;
+	delete selection;
+	delete vDrink;
+
+	system("PAUSE");
 	return 0;
 }
