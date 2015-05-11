7ab7c93b338a4cc44d946ad80052e152a5d1cdc4
Julian Grey <julianrgrey@gmail.com>
2015-05-11 14:48:38 +0100

    Add admin and data saving

--- VendingMachine/main.cpp
+++ VendingMachine/main.cpp
@@ -3,49 +3,68 @@
 #include <string>
 #include <vector>
 
-void addDrink(std::vector<Drink>* v); // Handle adding drinks manually
-void loadStock(); // Load stock from file
+void admin(std::vector<Drink>* v, bool* b);
+std::vector<Drink>* loadData(std::vector<Drink>* v); // Load stock from file
+void saveData(std::vector<Drink>* v); // Save stock to file
 
 int main() {
 	std::vector<Drink>* vDrink = new std::vector<Drink>;
-	Drink coke("Coke", 1.35, 10, vDrink);
-	(*vDrink).push_back(coke);
-	Drink sprite("Sprite", 1.05, 0, vDrink);
-	(*vDrink).push_back(sprite);
-	Drink fanta("Fanta", 1.25, 10, vDrink);
-	(*vDrink).push_back(fanta);
-
-	std::cout << "Welcome. What drink would you like to buy?\n\n";
-	for(int i = 0; i < (*vDrink).size(); i++) {
-		// Get a list of drinks which are in stock
-		if((*vDrink)[i].stock > 0) {
-			std::cout << (*vDrink)[i].name << " - " << (*vDrink)[i].price;
-			std::cout << " (selection: " << (*vDrink)[i].id << ")\n";
-		}
-	}
-	std::cout << '\n';
-
+	loadData(vDrink);
+	bool* bAdmin = new bool;
+	*bAdmin = false;
+	bool* quit = new bool;
+	*quit = false;
 	bool* found = new bool;
 	int* selection = new int;
-	std::cin >> *selection;
-	*found = false;
 
-	for(int i = 0; i < (*vDrink).size(); i++) {
-		if((*vDrink)[i].id == *selection) {
-			*found = true;
-			(*vDrink)[i].removeStock(); // This process handles out of stock
-			break;
+	while(!(*quit)) {
+		std::cout << "Welcome. What drink would you like to buy?\n\n";
+		for(size_t i = 0; i < (*vDrink).size(); i++) {
+			// Get a list of drinks which are in stock
+			if((*vDrink)[i].stock > 0) {
+				std::cout << (*vDrink)[i].name << " - " << (*vDrink)[i].price;
+				std::cout << " (selection: " << (*vDrink)[i].id << ")\n";
+			}
 		}
-	}
-	if(!(*found)) {
-		// Drink ID doesn't exist
-		std::cout << "Invalid selection.\n";
+		std::cout << "Admin (selection: -1)\n";
+		std::cout << "Quit (selection: 0)\n\n";
+		std::cin >> *selection;
+		std::cin.ignore();
+
+		switch(*selection) {
+			case 0:
+				*quit = true;
+				break;
+			case -1:
+				*bAdmin = true;
+				admin(vDrink, bAdmin);
+				break;
+		}
+
+		*found = false;
+		for(size_t i = 0; i < (*vDrink).size(); i++) {
+			if((*vDrink)[i].id == *selection) {
+				*found = true;
+				if((*vDrink)[i].stock > 0) {
+					std::cout << (*vDrink)[i].name << " dispensed. Enjoy your drink.\n\n";
+				}
+				(*vDrink)[i].removeStock(); // This process handles out of stock
+				break;
+			}
+		}
+		if(!(*found) && !(*quit)) {
+			// Drink ID doesn't exist
+			std::cout << "Invalid selection.\n";
+		}
+		std::cout << '\n';
 	}
 
+	saveData(vDrink); // Save changes to stock
+
+	delete bAdmin;
 	delete found;
 	delete selection;
+	delete quit;
 	delete vDrink;
-
-	system("PAUSE");
 	return 0;
 }
